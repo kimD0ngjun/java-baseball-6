992a8123a2a5fd143cec16f2ae2a30fd18976db0
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-22 15:01:33 +0900

    [feat] domain 입력값 타입 리스트화 메소

---
+++ src/main/java/baseball/domain/UserNumbers.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserNumbers {
+    private final List<Integer> userNumbers;
+
+    public UserNumbers(String inputNumber) {
+        this.userNumbers = changeType(inputNumber);
+    }
+
+    // type change
+    private List<Integer> changeType(String inputNumber) {
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int i = 0; i < inputNumber.length(); i++) {
+            char unitCharacter = inputNumber.charAt(i);
+
+            int digit = Character.getNumericValue(unitCharacter);
+            resultList.add(digit);
+        }
+
+        return resultList;
+    }
+
+    // validate
+}

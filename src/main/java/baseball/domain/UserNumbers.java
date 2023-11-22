7b7f88a57230edce76a22dbbc25106b4b190383e
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-22 17:01:06 +0900

    [refactoring] domain 입력값 유효성 검증 정규 표현식 교정 및 메소드 단축화

--- src/main/java/baseball/domain/UserNumbers.java
+++ src/main/java/baseball/domain/UserNumbers.java
@@ -7,8 +7,9 @@ import java.util.regex.Pattern;
 public class UserNumbers {
     private final List<Integer> userNumbers;
 
-    private final String CHECK_BLANK = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$";
-    private final int NOT_NUMBER = -1;
+    private final String VALIDATE = "^[1-9]*$";
+    ;
+    private final int LENGTH = 3;
 
     public UserNumbers(String inputNumber) {
         this.userNumbers = changeType(inputNumber);
@@ -21,16 +22,12 @@ public class UserNumbers {
 
     // type change
     private List<Integer> changeType(String inputNumber) {
-        validateBlank(inputNumber);
-        validateLength(inputNumber);
-
+        validate(inputNumber);
         List<Integer> resultList = new ArrayList<>();
 
         for (int i = 0; i < inputNumber.length(); i++) {
             char unitCharacter = inputNumber.charAt(i);
-
             int digit = Character.getNumericValue(unitCharacter);
-            validateNumberType(digit);
 
             resultList.add(digit);
         }
@@ -39,26 +36,24 @@ public class UserNumbers {
     }
 
     // validate
+    private void validate(String inputNumbers) {
+        checkLength(inputNumbers);
+        validateInputValue(inputNumbers);
+    }
+
     //Doesn't it have blank?
-    private void validateBlank(String inputNumbers) {
-        if (!Pattern.matches(CHECK_BLANK, inputNumbers) || inputNumbers.isBlank()) {
+    //Is its type a number?
+    // Is its range between 1 and 9?
+    private void validateInputValue(String inputNumbers) {
+        if (!Pattern.matches(VALIDATE, inputNumbers) || inputNumbers.isBlank()) {
             throw new IllegalArgumentException();
         }
     }
 
     //Is its length 3?
-    private void validateLength(String inputNumbers) {
+    private void checkLength(String inputNumbers) {
         if (inputNumbers.length() != 3) {
             throw new IllegalArgumentException();
         }
     }
-
-    //Is its type a number?
-    private void validateNumberType(int digit) {
-        if (digit == NOT_NUMBER) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    //TODO Is its range between 1 and 9?
 }

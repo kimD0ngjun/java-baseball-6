ca33e75bf65417aaab9b80cefe96b9a41adb4eef
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-22 16:32:14 +0900

    [feat] domain 입력값 유효성 검증 - 변환 문자 타입 검증

--- src/main/java/baseball/domain/UserNumbers.java
+++ src/main/java/baseball/domain/UserNumbers.java
@@ -8,6 +8,7 @@ public class UserNumbers {
     private final List<Integer> userNumbers;
 
     private final String CHECK_BLANK = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$";
+    private final int NOT_NUMBER = -1;
 
     public UserNumbers(String inputNumber) {
         this.userNumbers = changeType(inputNumber);
@@ -29,6 +30,8 @@ public class UserNumbers {
             char unitCharacter = inputNumber.charAt(i);
 
             int digit = Character.getNumericValue(unitCharacter);
+            validateNumber(digit);
+
             resultList.add(digit);
         }
 
@@ -36,21 +39,26 @@ public class UserNumbers {
     }
 
     // validate
-    //TODO Doesn't it have blank?
+    //Doesn't it have blank?
     private void validateBlank(String inputNumbers) {
         if (!Pattern.matches(CHECK_BLANK, inputNumbers) || inputNumbers.isBlank()) {
             throw new IllegalArgumentException();
         }
     }
 
-    //TODO Is its length 3?
+    //Is its length 3?
     private void validateLength(String inputNumbers) {
         if (inputNumbers.length() != 3) {
             throw new IllegalArgumentException();
         }
     }
 
-    //TODO Is its type a number?
+    //Is its type a number?
+    private void validateNumber(int digit) {
+        if (digit == NOT_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     //TODO Is its range between 1 and 9?
 }

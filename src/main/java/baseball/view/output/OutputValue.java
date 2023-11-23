d68399545376bfd47e146d9e32ad196ed13d14b2
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-23 20:42:32 +0900

    [feat] view 출력 인터페이스 구체화

--- src/main/java/baseball/view/output/OutputValue.java
+++ src/main/java/baseball/view/output/OutputValue.java
@@ -1,11 +1,25 @@
 package baseball.view.output;
 
 import baseball.domain.dto.CountDto;
+import baseball.view.message.OutputMessage;
 
 public class OutputValue implements Output {
     @Override
     public void outputCount(CountDto countDto) {
         final int ball = countDto.getBall();
         final int strike = countDto.getStrike();
+        outputBallStrike(ball, strike);
+    }
+
+    private void outputBallStrike(int ball, int strike) {
+        if (strike == 0) {
+            System.out.println(OutputMessage.BALL.getMessage(ball));
+        }
+        if (ball == 0) {
+            System.out.println(OutputMessage.STRIKE.getMessage(strike));
+        }
+        if (ball != 0 && strike != 0) {
+            System.out.println(OutputMessage.BALL_STRIKE.getMessage(ball, strike));
+        }
     }
 }

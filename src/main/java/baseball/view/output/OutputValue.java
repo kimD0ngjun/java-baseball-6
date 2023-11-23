205b0fbbbe327913bf755c648a4911d3eadb4d01
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-24 00:51:33 +0900

    [refactoring] view 결과 출력 메소드 분리

--- src/main/java/baseball/view/output/OutputValue.java
+++ src/main/java/baseball/view/output/OutputValue.java
@@ -8,16 +8,28 @@ public class OutputValue implements Output {
     public void outputCount(CountDto countDto) {
         final int ball = countDto.getBall();
         final int strike = countDto.getStrike();
+        outputResult(ball, strike);
+    }
+
+    private void outputResult(int ball, int strike) {
+        outputBall(ball, strike);
+        outputStrike(ball, strike);
         outputBallStrike(ball, strike);
     }
 
-    private void outputBallStrike(int ball, int strike) {
+    private void outputBall(int ball, int strike) {
         if (strike == 0) {
             System.out.println(OutputMessage.BALL.getMessage(ball));
         }
+    }
+
+    private void outputStrike(int ball, int strike) {
         if (ball == 0) {
             System.out.println(OutputMessage.STRIKE.getMessage(strike));
         }
+    }
+
+    private void outputBallStrike(int ball, int strike) {
         if (ball != 0 && strike != 0) {
             System.out.println(OutputMessage.BALL_STRIKE.getMessage(ball, strike));
         }
